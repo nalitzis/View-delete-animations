ea7f815d7994d45cde15f94b55964ed9317d6dbc
nalitzis <adolfo.bulfoni@gmail.com>
2012-10-13 16:47:48 +0200

    Initial Commit

---
+++ TestViewDelete/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestViewDelete
+//
+//  Created by Ado on 13/10/12.
+//  Copyright (c) 2012 Ado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TestViewDelete/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestViewDelete
+//
+//  Created by Ado on 13/10/12.
+//  Copyright (c) 2012 Ado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
